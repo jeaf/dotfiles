1c71ec203ea59d53c2d598f53f1449634b8a3d3c
Francois Jeannotte <francois.jeannotte@gmail.com>
2013-10-24 15:35:16 -0500

    Snapshot paquet.c in vim plugins.

--- vim/vimfiles/plugin/paquet.c
+++ vim/vimfiles/plugin/paquet.c
@@ -31,7 +31,8 @@ long base139_enc(const uint8_t* src, long srclen, uint8_t* dst, long dstlen)
     long rem  = srclen % SRC_BLK_SIZE;
     long pad  = rem > 0 ? (SRC_BLK_SIZE - rem) : 0;
     
-    assert(dstlen >= ((srclen + pad) / SRC_BLK_SIZE * DST_BLK_SIZE));
+    // Compute the required space for dst. The +1 is for the terminating null.
+    assert(dstlen >= (((srclen + pad) / SRC_BLK_SIZE * DST_BLK_SIZE) + 1));
 
     // Process whole blocks
     for (long i = 0; i < quot; ++i)
@@ -47,34 +48,30 @@ long base139_enc(const uint8_t* src, long srclen, uint8_t* dst, long dstlen)
     // Process last block (must be big-endian)
     if (pad > 0)
     {
-        uint64_t blk_little_endian = 0;
-        memcpy(&blk_little_endian, src + SRC_BLK_SIZE * quot, SRC_BLK_SIZE - pad);
         uint64_t blk = 0;
-
-        // Swap bytes to get big-endian
-        uint8_t* blkptr = (uint8_t*)&blk;
-        uint8_t* blk_little_endian_ptr = (uint8_t*)&blk_little_endian;
-        for (long k = 0; k < sizeof(uint64_t); ++k)
+        for (long k = 0; k < (SRC_BLK_SIZE - pad); ++k)
         {
-            blkptr[k] = blk_little_endian_ptr[sizeof(uint64_t) - k];
+            blk |= (uint64_t)(src + SRC_BLK_SIZE * quot)[k] << ((SRC_BLK_SIZE - k - 1) * 8);
         }
 
         for (long j = DST_BLK_SIZE - 1; j >= 0; --j)
         {
+            printf("blk: %llx\n", blk);
             (dst + quot * DST_BLK_SIZE)[j] = base139_symbols[blk % SYMBOL_COUNT];
             blk /= SYMBOL_COUNT;
         }
-
-        (dst + quot * DST_BLK_SIZE)[DST_BLK_SIZE - pad] = 0;
     }
+
+    // Put the terminating null
+    (dst + quot * DST_BLK_SIZE)[DST_BLK_SIZE - pad] = 0;
 
     return 0;
 }
 
 int main()
 {
-    uint8_t src[] = "abcdefghijklmnopqrstuvwxyz";
-    uint8_t dst[36] = {0};
+    uint8_t src[] = "abcdefgh";
+    uint8_t dst[37] = {0};
     base139_enc(src, strlen(src), dst, sizeof(dst));
     printf("Src    : %s\n", src);
     printf("Encoded: %s\n", dst);
