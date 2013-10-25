cfd573cdb5b83ffd2e41466de98f18f00a5b96e0
Francois Jeannotte <francois.jeannotte@gmail.com>
2013-10-25 13:46:14 -0500

    paquet.c snapshot.

--- vim/vimfiles/plugin/paquet.c
+++ vim/vimfiles/plugin/paquet.c
@@ -22,22 +22,22 @@ const long SYMBOL_COUNT = 139;
 const long SRC_BLK_SIZE = 8;
 const long ENC_BLK_SIZE = 9;
 
-void gen_invmap(const char* symbols)
-{
-    assert(symbols);
-    printf("const char base139_symbols_invmap[] = {");
-    long len = strlen(symbols);
-    char invmap[256] = {0};
-    for (long i = 0; i < len; ++i)
-    {
-        invmap[(uint8_t)symbols[i]] = i;
-    }
-    for (long j = 0; j < sizeof(invmap); ++j)
-    {
-        printf("%d,", invmap[j]);
-    }
-    printf("};\n");
-}
+//void gen_invmap(const char* symbols)
+//{
+//    assert(symbols);
+//    printf("const char base139_symbols_invmap[] = {");
+//    long len = strlen(symbols);
+//    char invmap[256] = {0};
+//    for (long i = 0; i < len; ++i)
+//    {
+//        invmap[(uint8_t)symbols[i]] = i;
+//    }
+//    for (long j = 0; j < sizeof(invmap); ++j)
+//    {
+//        printf("%d,", invmap[j]);
+//    }
+//    printf("};\n");
+//}
 
 long base139_enc(const uint8_t* src, long srclen, uint8_t* dst, long dstlen)
 {
@@ -66,7 +66,6 @@ long base139_enc(const uint8_t* src, long srclen, uint8_t* dst, long dstlen)
                        (uint64_t)blkptr[5] << 16 |
                        (uint64_t)blkptr[6] << 8  |
                        (uint64_t)blkptr[7];
-        printf("enc blk: %llx\n", blk);
         for (long j = ENC_BLK_SIZE - 1; j >= 0; --j)
         {
             (dst + i * ENC_BLK_SIZE)[j] = base139_symbols[blk % SYMBOL_COUNT];
@@ -74,7 +73,7 @@ long base139_enc(const uint8_t* src, long srclen, uint8_t* dst, long dstlen)
         }
     }
 
-    // Process last block (must be big-endian)
+    // Process last block
     if (pad > 0)
     {
         uint64_t blk = 0;
@@ -121,7 +120,6 @@ long base139_dec(const uint8_t* enc, long enclen, uint8_t* dst, long dstlen)
             uint8_t symbol = (enc + i * ENC_BLK_SIZE)[j];
             blk += base139_symbols_invmap[symbol] * powers_of_139[ENC_BLK_SIZE - j - 1];
         }
-        printf("dec blk: %llx\n", blk);
         uint8_t* dstptr = dst + i * SRC_BLK_SIZE;
         dstptr[0] = (blk & 0xFF00000000000000ULL) >> 56;
         dstptr[1] = (blk & 0x00FF000000000000ULL) >> 48;
@@ -133,43 +131,42 @@ long base139_dec(const uint8_t* enc, long enclen, uint8_t* dst, long dstlen)
         dstptr[7] = (blk & 0x00000000000000FFULL);
     }
 
-    // Process last block (must be big-endian)
-    //if (pad > 0)
-    //{
-    //    uint64_t blk = 0;
-    //    for (long k = 0; k < (SRC_BLK_SIZE - pad); ++k)
-    //    {
-    //        blk |= (uint64_t)(enc + SRC_BLK_SIZE * quot)[k] << ((SRC_BLK_SIZE - k - 1) * 8);
-    //    }
+    // Process last block
+    if (pad > 0)
+    {
+        uint64_t blk = 0;
+        for (long j = 0; j < ENC_BLK_SIZE; ++j)
+        {
+            uint8_t symbol = j < pad ? (enc + quot * ENC_BLK_SIZE)[j] : base139_symbols[SYMBOL_COUNT - 1];
+            blk += base139_symbols_invmap[symbol] * powers_of_139[ENC_BLK_SIZE - j - 1];
+        }
+        uint8_t* dstptr = dst + quot * SRC_BLK_SIZE;
+        dstptr[0] = (blk & 0xFF00000000000000ULL) >> 56;
+        dstptr[1] = (blk & 0x00FF000000000000ULL) >> 48;
+        dstptr[2] = (blk & 0x0000FF0000000000ULL) >> 40;
+        dstptr[3] = (blk & 0x000000FF00000000ULL) >> 32;
+        dstptr[4] = (blk & 0x00000000FF000000ULL) >> 24;
+        dstptr[5] = (blk & 0x0000000000FF0000ULL) >> 16;
+        dstptr[6] = (blk & 0x000000000000FF00ULL) >> 8;
+        dstptr[7] = (blk & 0x00000000000000FFULL);
+    }
 
-    //    for (long j = ENC_BLK_SIZE - 1; j >= 0; --j)
-    //    {
-    //        (dst + quot * ENC_BLK_SIZE)[j] = base139_symbols[blk % SYMBOL_COUNT];
-    //        blk /= SYMBOL_COUNT;
-    //    }
-    //}
-
-    // Put the terminating null and return size
-    //uint8_t* lastbyte = dst + quot * ENC_BLK_SIZE;
-    //if (pad > 0) lastbyte += ENC_BLK_SIZE - pad;
-    //*lastbyte = 0;
-    //return lastbyte - dst;
+    return quot * SRC_BLK_SIZE + pad;
 }
 
 int main()
 {
-    assert(base139_symbols_invmap['F'] == 34);
-    assert(base139_symbols_invmap['b'] == 61);
-    uint8_t src[] = "abcdefg";
+    uint8_t src[] = "aaaaaaaabbbbbbbbccccccccddddddddeeeeeeeeffff";
     //uint8_t src[] = {'a', 'b', 0, 0, 0, 0, 0, 0};
-    uint8_t dst[37] = {0};
-    uint8_t dec[30] = {0};
+    uint8_t dst[100] = {0};
+    uint8_t dec[100] = {0};
     //long enc_len = base139_enc(src, strlen(src), dst, sizeof(dst));
     long enc_len = base139_enc(src, sizeof(src), dst, sizeof(dst));
     printf("Encoded len: %d\n", enc_len);
     printf("Src    : %s\n", src);
     printf("Encoded: %s\n", dst);
-    base139_dec(dst, enc_len, dec, sizeof(dec));
+    long len = base139_dec(dst, enc_len, dec, sizeof(dec));
+    dec[len] = 0;
     printf("Decoded: %s\n", dec);
 
     return 0;
