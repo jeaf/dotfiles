bea6c012b479d692b790b30a7d7967b10cef6464
Francois Jeannotte <francois.jeannotte@gmail.com>
2013-10-25 14:07:34 -0500

    paquet.c: fixing padding problems, probs remaining.

--- vim/vimfiles/plugin/paquet.c
+++ vim/vimfiles/plugin/paquet.c
@@ -15,8 +15,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-const char* base139_symbols = "!#$%&()*+,-.0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz|~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓ";
-const char base139_symbols_invmap[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,3,4,0,5,6,7,8,9,10,11,0,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,0,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,0,86,0,87,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,-128,-127,-126,-125,-124,-123,-122,-121,-120,-119,-118,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+const uint8_t* base139_symbols = "!#$%&()*+,-.0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz|~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓ";
+const uint8_t base139_symbols_invmap[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,3,4,0,5,6,7,8,9,10,11,0,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,0,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,0,86,0,87,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,-128,-127,-126,-125,-124,-123,-122,-121,-120,-119,-118,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 uint64_t powers_of_139[] = {1, 139, 19321, 2685619ULL, 373301041ULL, 51888844699ULL, 7212549413161ULL, 1002544368429379ULL, 139353667211683681ULL};
 const long SYMBOL_COUNT = 139;
 const long SRC_BLK_SIZE = 8;
@@ -39,7 +39,7 @@ const long ENC_BLK_SIZE = 9;
 //    printf("};\n");
 //}
 
-long base139_enc(const uint8_t* src, long srclen, uint8_t* dst, long dstlen)
+void base139_enc(const uint8_t* src, long srclen, uint8_t* dst, long dstlen)
 {
     assert(strlen(base139_symbols) == SYMBOL_COUNT);
     assert(src);
@@ -50,6 +50,7 @@ long base139_enc(const uint8_t* src, long srclen, uint8_t* dst, long dstlen)
     long quot = srclen / SRC_BLK_SIZE;
     long rem  = srclen % SRC_BLK_SIZE;
     long pad  = rem > 0 ? (SRC_BLK_SIZE - rem) : 0;
+    printf("quot: %d, rem: %d, pad: %d\n", quot, rem, pad);
     
     // Compute the required space for dst. The +1 is for the terminating null.
     assert(dstlen >= (((srclen + pad) / SRC_BLK_SIZE * ENC_BLK_SIZE) + 1));
@@ -66,8 +67,10 @@ long base139_enc(const uint8_t* src, long srclen, uint8_t* dst, long dstlen)
                        (uint64_t)blkptr[5] << 16 |
                        (uint64_t)blkptr[6] << 8  |
                        (uint64_t)blkptr[7];
+        //printf("enc blk: %llu\n", blk);
         for (long j = ENC_BLK_SIZE - 1; j >= 0; --j)
         {
+            //printf("enc symb: %x, digit: %x\n", base139_symbols[blk % SYMBOL_COUNT], blk % SYMBOL_COUNT);
             (dst + i * ENC_BLK_SIZE)[j] = base139_symbols[blk % SYMBOL_COUNT];
             blk /= SYMBOL_COUNT;
         }
@@ -93,20 +96,20 @@ long base139_enc(const uint8_t* src, long srclen, uint8_t* dst, long dstlen)
     uint8_t* lastbyte = dst + quot * ENC_BLK_SIZE;
     if (pad > 0) lastbyte += ENC_BLK_SIZE - pad;
     *lastbyte = 0;
-    return lastbyte - dst;
 }
 
-long base139_dec(const uint8_t* enc, long enclen, uint8_t* dst, long dstlen)
+long base139_dec(const uint8_t* enc, uint8_t* dst, long dstlen)
 {
+    long enclen = strlen(enc);
     assert(enc);
     assert(enclen > 0);
-    assert(strlen(enc) == enclen);
     assert(dst);
 
     // Figure out padding
     long quot = enclen / ENC_BLK_SIZE;
     long rem  = enclen % ENC_BLK_SIZE;
     long pad  = rem > 0 ? (ENC_BLK_SIZE - rem) : 0;
+    printf("quot: %d, rem: %d, pad: %d\n", quot, rem, pad);
     
     // Compute the required space for dst
     assert(dstlen >= (((enclen + pad) / ENC_BLK_SIZE * SRC_BLK_SIZE)));
@@ -120,6 +123,7 @@ long base139_dec(const uint8_t* enc, long enclen, uint8_t* dst, long dstlen)
             uint8_t symbol = (enc + i * ENC_BLK_SIZE)[j];
             blk += base139_symbols_invmap[symbol] * powers_of_139[ENC_BLK_SIZE - j - 1];
         }
+        //printf("dec blk: %llu\n", blk);
         uint8_t* dstptr = dst + i * SRC_BLK_SIZE;
         dstptr[0] = (blk & 0xFF00000000000000ULL) >> 56;
         dstptr[1] = (blk & 0x00FF000000000000ULL) >> 48;
@@ -149,9 +153,11 @@ long base139_dec(const uint8_t* enc, long enclen, uint8_t* dst, long dstlen)
         dstptr[5] = (blk & 0x0000000000FF0000ULL) >> 16;
         dstptr[6] = (blk & 0x000000000000FF00ULL) >> 8;
         dstptr[7] = (blk & 0x00000000000000FFULL);
+
+        return (quot + 1) * SRC_BLK_SIZE - pad;
     }
 
-    return quot * SRC_BLK_SIZE + pad;
+    return quot * SRC_BLK_SIZE;
 }
 
 int main()
@@ -161,11 +167,13 @@ int main()
     uint8_t dst[100] = {0};
     uint8_t dec[100] = {0};
     //long enc_len = base139_enc(src, strlen(src), dst, sizeof(dst));
-    long enc_len = base139_enc(src, sizeof(src), dst, sizeof(dst));
-    printf("Encoded len: %d\n", enc_len);
+    printf("sizeof(src): %d\n", sizeof(src));
+    base139_enc(src, sizeof(src), dst, sizeof(dst));
+    printf("Encoded len: %d\n", strlen(dst));
     printf("Src    : %s\n", src);
     printf("Encoded: %s\n", dst);
-    long len = base139_dec(dst, enc_len, dec, sizeof(dec));
+    long len = base139_dec(dst, dec, sizeof(dec));
+    printf("Returned len: %d\n", len);
     dec[len] = 0;
     printf("Decoded: %s\n", dec);
 
